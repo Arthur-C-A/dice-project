35dee7b365798b846aee2145030db2c67c7c5847
Arthur C. A <159676770+Arthur-C-A@users.noreply.github.com>
2024-03-11 09:43:36 -0300

    Update main.js
    Add new feature: Says the winner

--- main.js
+++ main.js
@@ -3,8 +3,19 @@ function randomDice(){
     return Math.floor(number)
 }
 
-let pathPlayerOne = './images/dice' + randomDice() + '.png';
-let pathPlayerTwo = './images/dice' + randomDice() + '.png';
+//Programa Principal
+
+d1 = randomDice();
+d2 = randomDice();
+
+let pathPlayerOne = './images/dice' + d1 + '.png';
+let pathPlayerTwo = './images/dice' + d2 + '.png';
 
 let p1 = document.querySelector(".img1").setAttribute("src", pathPlayerOne);
 let p2 = document.querySelector(".img2").setAttribute("src", pathPlayerTwo);
+
+if( d1 > d2){
+    document.querySelector("h1").textContent = "🚩Player 1 Wins!";
+}else{
+    document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+}
